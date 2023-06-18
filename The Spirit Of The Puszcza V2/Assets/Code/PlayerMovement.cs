8fd210942a8ec27d66f3e0d83ca815223b906644
Miloszix <miloszmynarczuk@gmail.com>
2023-06-18 17:36:15 +0200

    pierwsza wersja jeszcze dużo do poprawki

---
+++ The Spirit Of The Puszcza V2/Assets/Code/PlayerMovement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5;
+
+    public Rigidbody2D rigidbody;
+
+    public Animator animator;
+
+    Vector2 movement;
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+    }
+
+    void FixedUpdate() 
+    {
+        rigidbody.MovePosition(rigidbody.position + movement * speed * Time.fixedDeltaTime);
+    }
+}

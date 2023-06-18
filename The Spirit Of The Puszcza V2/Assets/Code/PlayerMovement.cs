1f041e0edbd47489a05394424211d93c4cbff1d9
Miloszix <miloszmynarczuk@gmail.com>
2023-06-18 19:06:05 +0200

    tera chodzi jak powinien za myszką pog

--- The Spirit Of The Puszcza V2/Assets/Code/PlayerMovement.cs
+++ The Spirit Of The Puszcza V2/Assets/Code/PlayerMovement.cs
@@ -10,15 +10,30 @@ public class PlayerMovement : MonoBehaviour
 
     public Animator animator;
 
+    public Transform obj;
+    public Camera maincamera;
+    Vector2 objpos;
+    Vector2 mousepos;
+
+    float mousex;
+    float mousey;
+
     Vector2 movement;
     // Update is called once per frame
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+        
+        objpos = obj.transform.position;//gets player position
+	    mousepos = Input.mousePosition;//gets mouse postion
+	    mousepos = maincamera.ScreenToWorldPoint (mousepos);
+	    mousex = mousepos.x - objpos.x;//gets the distance between object and mouse position for x
+	    mousey = mousepos.y - objpos.y;//gets the distance between object and mouse position for y  if you want this.
 
-        animator.SetFloat("Horizontal", movement.x);
-        animator.SetFloat("Vertical", movement.y);
+
+        animator.SetFloat("Horizontal", mousex);
+        animator.SetFloat("Vertical", mousey);
         animator.SetFloat("Speed", movement.sqrMagnitude);
     }
 
